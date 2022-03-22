63c74b08af7ea1c1d13b7be927e8b40f203a81b5
solomonbarayev <sbarayev2@gmail.com>
2022-03-22 08:44:13 +0200

    add fixed form

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+const form = document.querySelector(".form");
+const profile = document.querySelector(".profile");
+let profileName = profile.querySelector(".profile__name");
+let profileTitle = profile.querySelector(".profile__title");
+let inputName = form.querySelector(".form__input[name='name']");
+let inputTitle = form.querySelector(".form__input[name='title']");
+
+const editProfileButton = profile.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const closeButton = popup.querySelector(".popup__close-button");
+
+///////////////
+//Event Handlers
+///////////////
+function toggleForm() {
+  inputName.value = profileName.textContent;
+  inputTitle.value = profileTitle.textContent;
+  popup.classList.toggle("popup_opened");
+}
+
+function formHandler(event) {
+  event.preventDefault();
+
+  profileName.textContent = inputName.value;
+  profileTitle.textContent = inputTitle.value;
+}
+
+///////////////
+//Event Listeners
+///////////////
+editProfileButton.addEventListener("click", toggleForm);
+closeButton.addEventListener("click", toggleForm);
+
+form.addEventListener("submit", formHandler);
