4bc25c9d61e00237da29a064f8f5a9f195a5dcf7
solomonbarayev <sbarayev2@gmail.com>
2022-04-04 13:39:01 +0300

    add inital cards with html template and js

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,51 @@ const closeButton = popup.querySelector(".popup__close-button");
 
 ////adding these to enable like button functionality
 const cards = document.querySelector(".cards");
+const cardList = cards.querySelector(".cards__list");
 const likesButtons = cards.querySelectorAll(".card__like-button");
+
+////Adding 6 initial cards
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
+function createCard(data) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardTitleElement = cardElement.querySelector(".card__title");
+  cardTitleElement.textContent = data.name;
+  const cardImg = cardElement.querySelector(".card__image");
+  cardImg.src = data.link;
+  return cardElement;
+}
+
+initialCards.forEach((card) => {
+  const cardElement = createCard(card);
+  cardList.prepend(cardElement);
+});
 
 ///////////////
 //Event Handlers
@@ -35,14 +79,14 @@ function formHandler(event) {
   profileTitle.textContent = inputTitle.value;
 }
 
-//loop for like button functionality
-for (let i = 0; i < likesButtons.length; i++) {
-  const likeButton = likesButtons[i];
-  function toggleLike() {
-    likeButton.classList.toggle("card__like-button_active");
-  }
-  likeButton.addEventListener("click", toggleLike);
-}
+// //loop for like button functionality
+// for (let i = 0; i < likesButtons.length; i++) {
+//   const likeButton = likesButtons[i];
+//   function toggleLike() {
+//     likeButton.classList.toggle("card__like-button_active");
+//   }
+//   likeButton.addEventListener("click", toggleLike);
+// }
 
 ///////////////
 //Event Listeners
